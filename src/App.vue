b2fb20e3c4027e9ca2a0c3682f1f22cb199a9d2f
xieyingcai <1365884247@qq.com>
2025-01-06 15:39:46 +0800

    添加多语言

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
-<script setup>
-</script>
-
 <template>
   <p>123</p>
   <div>456</div>
   <span class="yellow">789</span>
   <svgIcon customClass="customClass" name="svgTest"></svgIcon>
+  <!-- 中文显示姓名,英文显示name -->
+  <p>多语言:{{ $t("test") }}</p>
+  <p @click="changgeLanguageEn">切为英文</p>
+  <p @click="changgeLanguageZh">切为中文</p>
 </template>
+<script setup>
+import { useI18n } from "vue-i18n";
+import { ref } from 'vue'
+const { t, locale } = useI18n();
+const changgeLanguageEn = ()=>{
+  locale.value = 'en'; // 切换为英文
+}
+const changgeLanguageZh = ()=>{
+  locale.value = 'zh'; // 切换为英文
+}
+</script>
 
 <style lang="scss" scoped>
 p{
--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@ import '@/assets/styles/global.scss'
 import SvgIcon from '@/components/SvgIcon/SvgIcon.vue'
 // 引入自动生成的svg图标文件
 import 'virtual:svg-icons-register'
+// 引入i18n
+import i18n from "./locales/index.js";
 
 const app = createApp(App)
+app.use(i18n)
 app.component('SvgIcon', SvgIcon);
 app.mount('#app')
